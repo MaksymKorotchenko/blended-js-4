837a275d096f740f268a28a89ddc76578b683ac2
MaksymKorotchenko <maximuskor29@gmail.com>
2025-06-24 21:03:21 +0300

    upd

--- js/tasks.js
+++ js/tasks.js
@@ -94,16 +94,121 @@
 // for (let i = 1; i <= 100; i++) {
 //   const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 //   const div = document.createElement('div');
+
 //   div.classList.add('number');
 //   div.textContent = randomNumber();
+
 //   if (div.textContent % 2 === 0) {
 //     div.classList.add('even');
 //   } else {
 //     div.classList.add('odd');
 //   }
+
 //   container.append(div);
 // }
 
 /* ========================================================================================================================================================
 
 ======================================================================================================================================================== */
+
+// const form = document.querySelector('.contact-form');
+// const input = form.elements['userName'];
+// const span = document.querySelector('.js-username-output');
+
+// 1 - При події `input`, якщо користувач ввів в поле більше
+// 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+// то клас `error`
+
+// input.addEventListener('input', handleInput);
+// function handleInput(event) {
+//   if (event.currentTarget.value.trim().length < 6) {
+//     input.classList.add('error');
+//   } else if (event.currentTarget.value.trim().length >= 6) {
+//     input.classList.replace('error', 'success');
+//   }
+// }
+
+// 2 - При події `focus` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
+
+// input.addEventListener('focus', handleFocus);
+// function handleFocus(event) {
+//   if (event.currentTarget.value.trim() === '') {
+//     input.style.outline = '3px solid red';
+//   } else {
+//     input.style.outline = '3px solid green';
+//   }
+// }
+
+// 3 - При події `blur` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+// input.addEventListener('blur', handleBlur);
+// function handleBlur(event) {
+//   if (event.currentTarget.value.trim() === '') {
+//     input.style.outline = '3px solid red';
+//   } else {
+//     input.style.outline = '3px solid lime';
+//   }
+// }
+
+// 4 - При події `submit`. Відміни поведінку браузера по змовчуванню.
+// Дістань данні з інпуту і чек боксу, зроби перевірку,
+// що інпут не порожній, також, що нажатий чек бокс у положення true,
+// якщо користувач все виконав вірно, збери данні (userName)
+// у обьект і виведи у консоль. У разі, якщо користувач не виконав
+// одну із умов, виведи повідомлення. Також при події інпут реалізуй додавання
+// ім`я користувача у span, замість слова "Anonymous".
+// Якщо користувач ввів ім`я, а потім видалив, зроби так,
+// щоб на місце повернулось дефолтне знаяення "Anonymous".
+// При відправці форми, очисти інпут, верни чек бокс у положення
+// false, верни дефолтне значення "Anonymous" у span.
+
+// form.addEventListener('submit', handleSubmit);
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const input = form.elements['userName'],
+//     check = form.elements['accept'];
+
+//   if (input.value.trim() === '' || !check.checked) {
+//     return alert("Don't left empty and accept the policy");
+//   } else {
+//     const data = { userName: input.value.trim() };
+//     console.log(data);
+//     input.value = '';
+//     check.checked = false;
+//     span.textContent = 'Anonymous';
+//   }
+// }
+
+// input.addEventListener('input', handleInput);
+// function handleInput(event) {
+//   span.textContent = event.target.value.trim() || 'Anonymous';
+// }
+
+/* ========================================================================================================================================================
+
+======================================================================================================================================================== */
+
+// Використовуй шаблон розмітки з файлу html та напиши наступний функціонал:
+// При кліку на кнопку "Зменшити" квадрат стає меньшим на 20 пікселів,
+// При кліку на кнопку "Збільшити" - квадрат стає більшим на 20 пікселів.
+
+// const box = document.querySelector('.box');
+// const decreaseButton = document.querySelector('#decrease');
+// const increaseButton = document.querySelector('#increase');
+
+// const currentWidth = parseInt(getComputedStyle(box).width);
+// const currentHeigth = parseInt(getComputedStyle(box).height);
+
+// decreaseButton.addEventListener('click', event => {
+//   box.style.width = currentWidth - 20 + 'px';
+//   box.style.height = currentHeigth - 20 + 'px';
+// });
+
+// increaseButton.addEventListener('click', event => {
+//   box.style.width = currentWidth + 20 + 'px';
+//   box.style.height = currentHeigth + 20 + 'px';
+// });
